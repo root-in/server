bb27a48d78e189bac1d3c63bc56e56a55084f280
Reuven <rh@tufin.com>
2024-01-27 08:58:57 +0200

    AccessTypeOffline

--- internal/sheet.go
+++ internal/sheet.go
@@ -78,7 +78,7 @@ func getClient(config *oauth2.Config) (*http.Client, error) {
 
 // Request a token from the web, then returns the retrieved token.
 func getTokenFromWeb(config *oauth2.Config) (*oauth2.Token, error) {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOnline)
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
 	fmt.Printf("Go to the following link in your browser then type the "+
 		"authorization code: \n%v\n", authURL)
 
