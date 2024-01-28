11114b145ac56322c85e47e519e895923a824024
ShaniLavii <shanilavi@Tsafrirs-MacBook-Pro.local>
2024-01-28 15:27:02 +0200

    fixed top section image width

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,16 @@
 const form = document.querySelector('form');
 const submitMessage = document.querySelector('#submit-message');
+const loader = document.getElementById('loader');
+
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  loader.style.display = 'block';
   submitMessage.classList.add('show');
  
   setTimeout(() => {
     form.submit()
+    loader.style.display = 'none';
     form.reset();
   }, 2000);
 });
