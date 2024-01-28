3d01430298f07bab2c9e76ceaf5839325b1b7c05
ShaniLavii <shanilavi@Tsafrirs-MacBook-Pro.local>
2024-01-28 17:56:58 +0200

    added hamburger menu to mobile

--- www/js/index.js
+++ www/js/index.js
@@ -2,6 +2,8 @@ const form = document.querySelector('form');
 const submitMessage = document.querySelector('#submit-message');
 const loader = document.getElementById('loader');
 const inputs = form.querySelectorAll('input');
+const checkbox = document.querySelector('.checkbox');
+const menuItems = document.querySelector('.menu-items');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -20,4 +22,19 @@ inputs.forEach((input) => {
   input.addEventListener('focus', () => {
     submitMessage.classList.remove('show');
   });
+});
+
+
+document.addEventListener('click', (event) => {
+  if (
+    !checkbox.contains(event.target) &&
+    !menuItems.contains(event.target) &&
+    menuItems.classList.contains('open')
+  ) {
+    checkbox.checked = false;
+  }
+});
+
+checkbox.addEventListener('change', () => {
+  menuItems.classList.toggle('open', checkbox.checked);
 });
