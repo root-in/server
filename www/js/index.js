f9f625a817b5de2c4f712f2ff681e46c40404b0b
ShaniLavii <shanilavi@Tsafrirs-MacBook-Pro.local>
2024-01-28 15:06:51 +0200

    reset form fields after submit

--- www/js/index.js
+++ www/js/index.js
@@ -5,5 +5,8 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   submitMessage.classList.add('show');
  
-  setTimeout(() => form.submit(), 2000);
+  setTimeout(() => {
+    form.submit()
+    form.reset();
+  }, 2000);
 });
