ea469931ae0151064e3986a40222b4ae859ad136
Reuven <rh@tufin.com>
2024-01-29 10:52:01 +0200

    email is optional

--- internal/server.go
+++ internal/server.go
@@ -1,7 +1,6 @@
 package internal
 
 import (
-	"errors"
 	"fmt"
 	"io/fs"
 	"net/http"
@@ -20,64 +19,66 @@ func Run(content fs.FS) {
 func registerHandler(c *gin.Context) {
 	user, err := getUser(c)
 	if err != nil {
-		c.AbortWithError(http.StatusBadRequest, err)
+		c.AbortWithError(http.StatusBadRequest, fmt.Errorf("get user failed with %v", err))
 		return
 	}
 
 	fileHeader, err := c.FormFile("file")
 	if err != nil {
-		c.AbortWithError(http.StatusBadRequest, errors.New("can't read file"))
+		c.AbortWithError(http.StatusBadRequest, fmt.Errorf("can't read file for user %v", *user))
 		return
 	}
 
 	f, err := fileHeader.Open()
 	if err != nil {
-		c.AbortWithError(http.StatusBadRequest, errors.New("can't open file"))
+		c.AbortWithError(http.StatusBadRequest, fmt.Errorf("can't open file for user %v", *user))
 		return
 	}
 
 	fileName := user.getId()
 
 	if err := storeGCS(f, bucket, fileName); err != nil {
-		c.AbortWithError(http.StatusBadRequest, fmt.Errorf("failed to store file with %v", err))
+		c.AbortWithError(http.StatusBadRequest, fmt.Errorf("failed to store file for user %v with %v", *user, err))
 		return
 	}
 
 	if err := saveUser(user, fileName); err != nil {
-		c.AbortWithError(http.StatusBadRequest, fmt.Errorf("failed to save user with %v", err))
+		c.AbortWithError(http.StatusBadRequest, fmt.Errorf("failed to save user %v with %v", *user, err))
 		return
 	}
 
-	c.String(http.StatusOK, fmt.Sprintf("'%s' registered", user))
+	fmt.Printf("Registered: %v\n", *user)
 }
 
 func getUser(c *gin.Context) (*User, error) {
 	var user User
 	var err error
 
-	if user.Name, err = getField(c, "name"); err != nil {
-		return nil, c.AbortWithError(http.StatusBadRequest, err)
+	if user.Name, err = getMandatoryField(c, "name"); err != nil {
+		return nil, err
 	}
 
-	if user.Surname, err = getField(c, "surname"); err != nil {
-		return nil, c.AbortWithError(http.StatusBadRequest, err)
+	if user.Surname, err = getMandatoryField(c, "surname"); err != nil {
+		return nil, err
 	}
 
-	if user.Email, err = getField(c, "email"); err != nil {
-		return nil, c.AbortWithError(http.StatusBadRequest, err)
-	}
+	user.Email = getOptionalField(c, "email")
 
-	if user.Phone, err = getField(c, "phone"); err != nil {
-		return nil, c.AbortWithError(http.StatusBadRequest, err)
+	if user.Phone, err = getMandatoryField(c, "phone"); err != nil {
+		return nil, err
 	}
 
 	return &user, nil
 }
 
-func getField(c *gin.Context, name string) (string, error) {
+func getMandatoryField(c *gin.Context, name string) (string, error) {
 	result := c.PostForm(name)
 	if result == "" {
 		return "", fmt.Errorf("field %q is missing", name)
 	}
 	return result, nil
+}
+
+func getOptionalField(c *gin.Context, name string) string {
+	return c.PostForm(name)
 }
